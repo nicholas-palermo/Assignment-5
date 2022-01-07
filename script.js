0108148b9df5fcd0bc87f190dffad58b55106e02
Nick Palermo <nicholas.palermo8@gmail.com>
2022-01-07 16:58:28 -0500

    feature added: fill all white boxes

--- script.js
+++ script.js
@@ -10,6 +10,20 @@ $(document).ready(function () {
         $(this).addClass(color);
     })
 
+    //Fill all white boxes with selected color
+    $('#fillWhite').click(function () {
+        const whiteBoxes = document.querySelectorAll(".box" && ".white")
+        const fillColor = document.querySelector("#colorChoiceAllWhite").value;
+
+        whiteBoxes.forEach(element => {
+            element.classList.remove('white');
+            element.classList.add(fillColor);
+        });
+
+        $(whiteBoxes).css('background-color', fillColor)
+    }) 
+
+
 
 })
 
